b278a1399ce29c37355460e294fd19da2d4a482f
Cobra30621 <44718953+Cobra30621@users.noreply.github.com>
2018-11-20 21:17:19 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 18:12:45 2018
+
+@author: linzino
+"""
+# server-side
+from flask import Flask, request, abort
+
+# line-bot
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+# package
+import re
+from datetime import datetime 
+
+# customer module
+import mongodb
+import corwler
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('dCdBEwGwdWUxethKTVf5nZX8CMpaY7zU0k44dvBp9V8S6iacj3LZWUSx9DG0WuXxbGBaU6I5QjoOilzZ1WOeiuF4vQY0nzbzSyhoGh/UE3uUD7HBW6b3JUu2mWAp2s/i+dLBcv2e/eGasmUYYOwRAgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('43f859131a0ee481594c57358b4d330e')
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+
+    
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    '''
+    當使用者加入時觸動
+    '''
+    # 取得使用者資料
+    profile = line_bot_api.get_profile(event.source.user_id)
+    name = profile.display_name
+    uid = profile.user_id
+    
+    print(name)
+    print(uid)
+    # Udbddac07bac1811e17ffbbd9db459079
+    if mongodb.find_user(uid,'users')<= 0:
+        # 整理資料
+        dic = {'userid':uid,
+               'username':name,
+               'creattime':datetime.now(),
+               'Note':'user',
+               'ready':0}
+        
+        mongodb.insert_one(dic,'users')
+   
+def makeCard(dic, event):
+    dic = dic
+
+    columns = []
+    for i in range(3):
+        carousel = CarouselColumn(
+                    thumbnail_image_url = dic[i]['img'],
+                    title = dic[i]['title'],
+                    text = dic[i]['summary'],
+                    actions=[
+                        URITemplateAction(
+                            label = '點我看新聞',
+                            uri = dic[i]['link']
+                          )
+                        ]
+                    )
+        columns.append(carousel)
+    
+    remessage = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(columns=columns)
+                )
+    
+    
+    line_bot_api.reply_message(event.reply_token, remessage)      
+        
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    '''
+    當收到使用者訊息的時候
+    '''
+    profile = line_bot_api.get_profile(event.source.user_id)
+    name = profile.display_name
+    uid = profile.user_id
+    message = event.message.text
+    print(name)
+    print(uid)
+    print(message)
+    
+    dic = {'userid':uid,
+       'username':name,
+       'creattime':datetime.now(),
+       'mess':message}
+    mongodb.insert_one(dic,'message')
+    
+    if mongodb.get_ready(uid,'users') ==1 :
+        mongodb.update_byid(uid,{'ready':0},'users')
+        casttext = name+' 對大家說： '+message
+        remessage = TextSendMessage(text=casttext)
+        userids = mongodb.get_all_userid('users')
+        line_bot_api.multicast(userids, remessage)
+        return 0 
+
+
+    if message == '群體廣播':
+        # 設定使用者下一句話要群廣播
+        mongodb.update_byid(uid,{'ready':1},'users')
+        remessage = TextSendMessage(text='請問要廣播什麼呢?')
+        line_bot_api.reply_message(
+                        event.reply_token,
+                        remessage)
+        return 0 
+    
+    if re.search('Hi|hello|你好|ha', message, re.IGNORECASE):
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+        
+        return 0 
+#關鍵評論網
+            
+    if re.search('theNewLens', event.message.text, re.IGNORECASE):
+     
+        columns = []
+        img = 'https://image3.thenewslens.com/assets/web/cover-photo-medium.png'
+
+        carousel = CarouselColumn(
+                    thumbnail_image_url = img,
+                    title = '關鍵評論網新聞',
+                    text = '點擊觀看類型',
+                    actions=[
+                        MessageTemplateAction(
+                            label='科學',
+                            text='關鍵評論科學'
+                           ),
+                        MessageTemplateAction(
+                            label='商業',
+                            text='關鍵評論商業'
+                           ),
+                        MessageTemplateAction(
+                            label='職場',
+                            text='關鍵評論職場'
+                           )
+                         ]
+                     )
+        columns.append(carousel)
+        
+        remessage = TemplateSendMessage(
+                    alt_text='Carousel template',
+                    template=CarouselTemplate(columns=columns)
+                    )
+        
+        
+        line_bot_api.reply_message(event.reply_token, remessage)
+        return 0         
+            
+    
+    
+    if re.search('關鍵評論科學', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('science')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('關鍵評論商業', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('business')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('關鍵評論職場', event.message.text, re.IGNORECASE):
+        dic = corwler.theNewLens('career')
+        
+        makeCard(dic, event)
+        return 0 
+
+#科技報橘
+        
+    if re.search('科技報橘', event.message.text, re.IGNORECASE):
+     
+        columns = []
+        img = 'https://asia.money2020.com/sites/asia.money2020.com/files/Tech-orange360.png'
+
+        carousel = CarouselColumn(
+                    thumbnail_image_url = img,
+                    title = '科技報橘新聞',
+                    text = '點擊觀看類型',
+                    actions=[
+                        MessageTemplateAction(
+                            label='創新創業',
+                            text='tech創新創業'
+                           ),
+                        MessageTemplateAction(
+                            label='人工智慧',
+                            text='tech人工智慧'
+                           ),
+                        MessageTemplateAction(
+                            label='數位行銷',
+                            text='tech數位行銷'
+                           )
+
+                         ]
+                     )
+        columns.append(carousel)
+
+        
+        remessage = TemplateSendMessage(
+                    alt_text='Carousel template',
+                    template=CarouselTemplate(columns=columns)
+                    )
+        
+        
+        line_bot_api.reply_message(event.reply_token, remessage)
+        return 0         
+            
+    
+    if re.search('tech創新創業', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('創新創業/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('tech人工智慧', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('artificialintelligence/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('techorange新經濟', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('新經濟/')
+        
+        makeCard(dic, event)        
+        return 0 
+    
+    if re.search('techorange數位轉型', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('數位轉型/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('techorange', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('')
+        
+        makeCard(dic, event)
+        return 0 
+    
+    if re.search('tech數位行銷', event.message.text, re.IGNORECASE):
+        dic = corwler.techorange('software_digimarketing/')
+        
+        makeCard(dic, event)
+        return 0 
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ corwler.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 00:17:32 2018
+
+@author: linzino
+"""
+import requests #引入函式庫
+from bs4 import BeautifulSoup
+import re
+import json
+import feedparser
+
+
+def udn_news():
+    '''
+    抓最新前三篇新聞
+    
+    回傳是一個dict
+    '''
+    rss_url = 'https://udn.com/rssfeed/news/2/6638?ch=news'
+ 
+    # 抓取資料
+    rss = feedparser.parse(rss_url)
+    
+    cards = []    
+    for index in range(0,3):
+        # 抓文章標題
+        title = rss['entries'][index]['title']
+        # 抓文章連結
+        link = rss['entries'][index]['link']
+        # 處理摘要格式
+        summary =  rss['entries'][index]['summary']
+        
+        if 'img' in summary:
+            soup = BeautifulSoup(summary, 'html.parser')
+            p_list = soup.find_all('p')
+            # 抓文章摘要 限制只有60個字
+            text = p_list[1].getText()[:50]
+            # 抓文章圖片
+            image = p_list[0].img['src']
+        else:
+            # 沒有圖片
+            text = summary[:50]
+            image = 'https://i.imgur.com/vkqbLnz.png'
+        
+        card = {'title':title,
+                'link':link,
+                'summary': text,
+                'img':image
+                }
+        cards.append(card)
+        
+    return cards
+
+
+
+
+def techorange(newType):
+    '''
+    在techorangeAi 上某個關鍵字最新的文章
+    '''
+    newType = newType
+    url = 'https://buzzorange.com/techorange/tag/' + newType
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    atags = soup.select('.entry-title')
+
+    cards = []
+    for index in range(3):    
+        #文章標題
+
+        title = atags[index].text[:40]
+        
+        #文章內文
+        interUrl = atags[index].a['href']
+        interResp = requests.get(interUrl)
+        interSoup = BeautifulSoup(interResp.text, 'html.parser')
+        interAtags = interSoup.select('.entry-content')
+        text = interAtags[0].text.replace('\n', '')[:50]
+
+        
+        #文章連結
+        link = interUrl
+        
+        #圖片
+        img = interSoup.find_all('img', re.compile('align'))
+        image = img[1]['src']   
+        
+        card = {'title':title,
+                    'link':link,
+                    'summary': text,
+                    'img':image
+                    }
+        cards.append(card)
+ 
+    
+    return cards
+    
+
+
+def theNewLens(newType):
+    '''
+    搜尋關鍵評論網（theNewLens）的科學文章，做成字卡
+    '''
+    newType = 'career'
+    url = 'https://www.thenewslens.com/category/' + newType
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    titles = soup.select('.title')
+    texts = soup.select('.description')  
+    links = soup.select('.img-box') 
+    images = soup.select('.lazy-container') 
+
+    cards = []
+
+    for index in range(3):
+        #文章標題
+        title = titles[index].text[:40]
+        
+        #文章連結
+        link = links[index].a['href']
+        
+        #文章內文
+        text = texts[index].text[:50] 
+        
+        #圖片
+        image = re.findall(r'(https.*?)\d{3,}w',str(images[index]))[2]
+        #b = re.findall('350w,[\S]*400w',str(images[0]))
+        
+        
+        
+        card = {'title':title,
+                        'link':link,
+                        'summary': text,
+                        'img':image
+                        }
+        cards.append(card)
+
+    return cards
+
+def fb():
+    '''
+    搜尋關鍵評論網（theNewLens）的科學文章，做成字卡
+    '''
+    url = 'https://www.facebook.com/chasedrea?__tn__=%2CdC-R-R&eid=ARDnjjnJesfZGk4eRSXvv3mzSi9A3t5sZ3KokokeDUJH-COyd6dwjMdjxYTfsMjVQ3GygfVOZrgr54r-&hc_ref=ARRXWEZo5GRi3kCiaSIvgsDW0gdAdO5hexa2T6cM3rpCfCRPAtEiepFoBKhi_kNLLT8&fref=nf'
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    atags = soup.find_all('div', re.compile('_26ni hidden_elem img'))
+
+    
+    cards = []
+    for index in range(3):
+        #文章標題
+        title = atags2[index].a['title']
+        
+        #文章連結
+        link = atags2[index].a['href']
+        
+        #文章內文
+        textlist = atags[index].find_all('div', re.compile('description'))
+        text = str(textlist[0])
+        text = text.replace('<div class="description"> ', '')[:50] 
+        
+        #圖片
+        
+        imglist = str(atags2[index].a.div.img).split(',')
+        imglist = imglist[2].split(' ')
+        image = imglist[1]
+        
+        
+        card = {'title':title,
+                        'link':link,
+                        'summary': text,
+                        'img':image
+                        }
+        cards.append(card)
+
+    return cards
+
+
